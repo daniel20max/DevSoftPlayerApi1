d99ecd3a3cc39c0f175d418fad4e333e9e1685d8
daniel20max <51431192+daniel20max@users.noreply.github.com>
2021-08-06 18:05:59 -0300

    Initial commit

---
+++ Controllers/TaxaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevSoftPlayerApi1.Controllers
+{
+    [ApiController]
+    [Route("Index")]
+    public class TaxaController : Controller
+    {
+        [HttpGet]
+        [Route("TaxaJuros")]
+        public IActionResult TaxaJuros()
+        {
+            var taxa = 0.01;
+            return Ok(taxa);
+        }
+    }
+}
